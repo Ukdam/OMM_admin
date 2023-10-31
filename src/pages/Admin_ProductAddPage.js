daf7ffc3ba6a0e22a4fce63f48e0092f9ec99095
1Taron <she11466@naver.com>
2023-10-31 20:29:27 +0900

    바뀐거 업데이트

--- src/pages/Admin_ProductAddPage.js
+++ src/pages/Admin_ProductAddPage.js
@@ -1,14 +1,29 @@
 import "../css/Admin_ProductAddPage.css"
-import { Switch } from '@chakra-ui/react'
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Admin_ProductAddPage() {
+
+
+
+    //전시 상태 true or false
+    const [isChecked, setIsChecked] = useState(false);
+
+    const P_CheckedHandler = (e) => {
+        setIsChecked(e.target.checked);
+    };
+
+
+
     return (
         <>
             <form className="main_outlet font_01">
                 <div className="ProductAdd_Page">상품상세</div>
                 <hr />
                 <div className="ProductAdd_Btn_Container">
-                    <button className="font_01">목록보기</button>
+                    <Link to={"/product"}>
+                        <button className="font_01">목록보기</button>
+                    </Link>
                     <button className="font_01">삭제</button>
                     <button className="font_01">저장</button>
                 </div>
@@ -44,8 +59,14 @@ export default function Admin_ProductAddPage() {
                         </div>
                         <div className="input_Info_P">
                             <span>판매상태</span>
-
-
+                            <label className="switch_P">
+                                <input
+                                    type="checkbox"
+                                    checked={isChecked}
+                                    onChange={P_CheckedHandler}
+                                />
+                                <span className="slider_P"></span>
+                            </label>
                         </div>
                         <div className="input_Info_P">
                             <span>판매가</span>
