80c07f1b260abb1cc42aed4bb1f01f05c7f93a17
PARK JI GANG <90240682+qkrwlrkd@users.noreply.github.com>
2023-11-16 03:01:49 +0900

    디테일페이지(기능 제외)

--- src/pages/Admin_UserDetailPage.js
+++ src/pages/Admin_UserDetailPage.js
@@ -1,11 +1,84 @@
+import "../css/Admin_UserDetailPage.css";
+
+
 export default function Admin_UserDetailPage() {
-    return (
-      <>
-        <main className="main_outlet font_01">
-          <div className="user-title-container">회원</div>
-          
-        </main>
-        
-      </>
-    );
+  function UserPage() {
+    window.location = '/user'
   }
+  return (
+    <>
+      <main className="main_outlet font_01">
+        <div className="user-title-container">회원상세정보</div>
+        <button className="D_cancle_btn font_01" onClick={UserPage}>취소</button>
+        <button className="D_save_btn font_01 ">저장</button>
+        <div className="detailcontainer">
+          <div className="detailcontainer1">
+            <p>프로필</p>
+            <hr />
+            <div>
+              <p className="no-margin">
+                <span className="profile-font1">회원가입일</span>
+                <input
+                  className="profile-font"
+                  placeholder="2023-09-24"
+                  value={""}
+                  type="text"
+                ></input>
+              </p>
+              <p className="no-margin">
+                <span className="profile-font1">아이디</span>
+                <input
+                  className="profile-font"
+                  placeholder="test1"
+                  value={""}
+                  type="text"
+                ></input>
+              </p>
+              <p className="no-margin">
+                <span className="profile-font1">이메일주소</span>
+                <input
+                  className="profile-font"
+                  placeholder="example@gmail.com"
+                  value={""}
+                  type="text"
+                ></input>
+              </p>
+              <p className="no-margin">
+                <span className="profile-font1">이름</span>
+                <input
+                  className="profile-font"
+                  placeholder="test1"
+                  value={""}
+                  type="text"
+                ></input>
+              </p>
+              <p className="no-margin">
+                <span className="profile-font1">휴대전화</span>
+                <input
+                  className="profile-font"
+                  placeholder="010-0000-0000"
+                  value={""}
+                  type="text"
+                ></input>
+              </p>
+            </div>
+          </div>
+          <div className="detailcontainer-right">
+            <div className="detailcontainer2">
+              <p>마케팅 광고 수신 동의</p>
+              <hr />
+              <p className="profile-font1">이메일</p>
+              <p className="profile-font1">sms</p>
+            </div>
+            <div className="detailcontainer3">
+              <p>선택적 동의 항목</p>
+              <hr />
+              <p className="profile-font1">개인정보 수집 동의</p>
+            </div>
+          </div>
+        </div>
+      </main>
+
+    </>
+  );
+}
--- src/pages/Admin_UserPage.js
+++ src/pages/Admin_UserPage.js
@@ -1,18 +1,38 @@
 import "../css/Admin_UserPage.css";
-import Table from "../compoents/Admin_Table";
-import SearchBox from "../compoents/SearchBox";
+
 
 export default function Admin_UserPage() {
+  function UserDetialPage() {
+    window.location = '/user_detail'
+  }
   return (
     <>
       <main className="main_outlet font_01">
         <div className="user-title-container">회원</div>
-        <SearchBox />
-        
-    <Table />
-  
+        <div className="user-search-container">
+          <div className="searchbox">
+            <p>아이디, 이메일, 이름</p>
+          </div>
+          <button className="search-button">검색</button>
+          <button className="per-page-button">10개씩</button>
+        </div>
+
+        <div className="Usertable_Line1">
+          <span>관리</span>
+          <span>아이디</span>
+          <span>이름</span>
+          <span>휴대번호</span>
+          <span>회원가입일</span>
+        </div>
+        <div className="U_list_container">
+          <button className="U_detail_btn" onClick={UserDetialPage}>상세</button>
+          <span>test01</span>
+          <span>장씨</span>
+          <span>010-1234-5678</span>
+          <span>2034-09-19</span>
+        </div>
       </main>
-      
+
     </>
   );
 }
